d03f1432cd5855f4bf7d16e500a7e99625620057
crownVic85 <victor0285@yahoo.com>
2023-04-30 17:30:45 -0700

    update solution

--- rest spread exercises.js
+++ rest spread exercises.js
@@ -64,6 +64,8 @@ const combine = (obj1, obj2) => {
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {
-
+const update = (obj, key, val) => {
+    let newObj = { ...obj };
+    newObj[key] = val;
+    return newObj;
 }
