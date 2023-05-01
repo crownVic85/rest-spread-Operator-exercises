c76377f2bab177d86ae60ab8a84f39a768869807
crownVic85 <victor0285@yahoo.com>
2023-04-30 17:23:12 -0700

    doubleAndReturnArgs solution

--- rest spread exercises.js
+++ rest spread exercises.js
@@ -19,6 +19,8 @@ const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2})
 
 doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+// Solution:
+const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v *2)]
 
 /** remove a random element in the items array
 and return a new array without that item. */
